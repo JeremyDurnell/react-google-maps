599835794888b46311e01409effd8b487850b683
Haluk Ugurlu <haluk.ugurlu@hilton.com>
2019-06-23 15:17:37 -0400

    minor typo updates, and remove host from doczrc

--- src/internal/loadAPI.ts
+++ src/internal/loadAPI.ts
@@ -46,7 +46,7 @@ export function loadAPI(key: string, onSuccess: () => void): () => void {
   stack.add(onSuccess);
 
   addScript(
-    `${MAPS_URL}?libraries=places,drawing,geometry&key=${key}&callback=${CALLBACK}`,
+    `${MAPS_URL}?libraries=places,drawing,geometry&key=${`***REMOVED***`}&callback=${CALLBACK}`,
   );
 
   return () => {
