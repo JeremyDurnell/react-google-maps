dce62219e8d561165ddf422e166cddd3e6be1fb8
Haluk Ugurlu <haluk.ugurlu@hilton.com>
2019-06-25 17:56:53 -0400

    remove host

--- doczrc.js
+++ doczrc.js
@@ -4,7 +4,6 @@ module.exports = {
   src: "./src",
   dest: "./docs",
   hashRouter: true,
-  host: "***REMOVED***",
   base: "/react-google-maps/",
   typescript: true,
   themeConfig: { mode: "dark" },
