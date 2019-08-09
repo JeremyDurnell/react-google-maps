e55ca88d155a1ac772189fbaf8656394993179b7
Haluk <halukugurlu@gmail.com>
2019-08-09 15:13:54 -0400

    Marker Clusterer v1 (#6)
    * Marker Clusterer v1
    
    * typo

---
+++ src/marker-clusterer/MarkerClusterer.tsx
@@ -0,0 +1,46 @@
+import React, { ReactElement, useEffect } from "react";
+import * as MarkerClustererPlus from "@google/markerclustererplus";
+
+import {
+  GoogleMapOverlayViewContext,
+  useGoogleMap,
+} from "../context/GoogleMapsContext";
+
+
+export interface MarkerClustererProps {
+  /**
+   * MarkerClusterer locations.
+   */
+  locations: Array<google.maps.LatLngLiteral>;
+ /**
+   * MarkerClusterer options.
+   */
+  options: MarkerClustererOptions;
+}
+
+export function MarkerClusterer({
+  locations,
+  options,
+}: MarkerClustererProps): null | ReactElement<object> {
+  /**
+   * Instantiate Google Map.
+   */
+  const map = useGoogleMap();
+
+  useEffect(() => {
+    if (!map) {
+      return;
+    }
+    // Generate markers to be clustered.
+    const markers = locations.map(function(location) {
+      return new google.maps.Marker({
+        position: location,
+      });
+    });
+
+    // Add a marker clusterer to manage the markers.
+    new MarkerClustererPlus(map, markers, options);
+  }, [map]);
+
+  return <GoogleMapOverlayViewContext.Provider value={null} />;
+}
